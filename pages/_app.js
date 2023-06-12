ac16298c9122dfeebc5f24a9f37cbd306c24898e
AthenasCode <emoore5203@gmail.com>
2023-06-12 08:55:40 +0800

    added google analytics script

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,31 @@
 // pages/_app.js
 import "../styles/global.css";
-import { Lora } from "next/font/google"
+import { Lora } from "next/font/google";
+import Script from "next/script";
 
-const lora = Lora({ subsets: ['latin'] })
+const lora = Lora({ subsets: ["latin"] });
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />
+      <Script
+        strategy="lazyOnload"
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+      />
+
+      <Script strategy="lazyOnload">
+        {`
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+                    gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+                    page_path: window.location.pathname,
+                    });
+                `}
+      </Script>
+    </>
+  );
 }
 
 // Component is the active page - whenever you navigate between routes, Component will change to the new page. Any props sent to Component will be received by the page.
