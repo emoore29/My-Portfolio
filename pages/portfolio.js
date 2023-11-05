389239e0259a23149267d3b43f061c9c82b28f0b
AthenasCode <emoore5203@gmail.com>
2023-11-05 12:10:07 +0800

    added responsive styling for portfolio

--- pages/portfolio.js
+++ pages/portfolio.js
@@ -2,14 +2,24 @@ import utilStyles from "../styles/utils.module.css";
 import styles from "../styles/Home.module.css";
 import Image from "next/image";
 import { useState } from "react";
-import HoverBlur from "../components/hoverBlur";
-import { lora } from "../styles/fonts";
 import Layout from "../components/layout";
+import Link from "next/link";
+import ListItem from "../components/icons/listIcon";
 
 export default function Portfolio() {
   const [activeProject, setActiveProject] = useState(null);
 
   const projectLinks = [
+    {
+      id: "manuscriptApp",
+      github: "https://github.com/ShayneJG/Manuscript-Manager",
+      live: "https://manuscript-manager.vercel.app/",
+      title: "Manuscript Manager",
+      tech: "Next.js, Auth0",
+      alt: "Manuscript Manager",
+      description: "An app that tracks monthly freelance earnings.",
+      src: "manuscript-manager-screenshot.png",
+    },
     {
       id: "artPortfolio",
       github: null,
@@ -19,38 +29,6 @@ export default function Portfolio() {
       alt: "Art Portfolio",
       description: "A six-page static website.",
       src: "dp-portfolio-screenshot.jpg",
-    },
-    {
-      id: "pomodoroTimer",
-      github: "https://github.com/AthenasCode/Pomodoro",
-      live: "https://athenascode.github.io/Pomodoro/",
-      title: "Pomodoro Timer",
-      tech: "React",
-      alt: "Pomodoro Timer",
-      description: "A minimal, customisable timer.",
-      src: "pomo-screenshot.jpg",
-    },
-
-    {
-      id: "cardDetails",
-      github: "https://github.com/AthenasCode/Interactive-Card-Details-Form",
-      live: "https://athenascode.github.io/Interactive-Card-Details-Form/",
-      title: "Interactive Card Details Form",
-      tech: "Tailwind, React",
-      alt: "Interactive Card Details Form",
-      description: "A form that displays card details as the user types.",
-      src: "interactive-card-details-screenshot.jpg",
-    },
-    {
-      id: "manuscriptApp",
-      github: "https://github.com/ShayneJG/Manuscript-Manager",
-      live: "https://manuscript-manager.vercel.app/",
-      title: "Manuscript Manager",
-      tech: "Next.js, Auth0",
-      alt: "Manuscript Manager",
-      description:
-        "A collaborative project with a fellow programmer for an app that tracks monthly freelance earnings.",
-      src: "manuscript-manager-screenshot.png",
     },
   ];
 
@@ -62,40 +40,44 @@ export default function Portfolio() {
         </h2>
         <ul className={styles.portfolioItems}>
           {projectLinks.map((project) => (
-            <li
-              id={project.id}
-              onMouseOver={() => setActiveProject(project.id)}
-              onMouseOut={() => setActiveProject(null)}
-              className={styles.projectContainer}
-            >
-              <Image
-                width={500}
-                height={500}
-                alt="Picture of the author"
-                src={`/images/${project.src}`}
-              />
-              <p className={styles.projectName}>{project.title}</p>
-              <p className={styles.projectTech}>{project.tech}</p>
-              {/* <div className={styles.projectImageParent}>
-                <Image
-                  src={"/images/" + project.src}
-                  fill
-                  className={`${utilStyles.objectContain} ${styles.projectImage}`}
-                  alt={project.alt}
-                />
-              </div>
-              <p className={`${styles.projectTitle}`}>{project.title}</p>
-              <p
-                className={`${utilStyles.italic} ${lora.className} ${styles.projectDescription}`}
+            <>
+              <li
+                id={project.id}
+                onMouseOver={() => setActiveProject(project.id)}
+                onMouseOut={() => setActiveProject(null)}
+                className={styles.projectContainer}
               >
-                {project.description}
-              </p>
-              <HoverBlur
-                isActive={activeProject === project.id}
-                github={project.github}
-                live={project.live}
-              /> */}
-            </li>
+                <div className={styles.portfolioImageWrapper}>
+                  <Image
+                    fill
+                    alt={project.alt}
+                    src={`/images/${project.src}`}
+                  />
+                </div>
+                {/* <p>View details... dropdown coming soonTM</p> */}
+                <section className={styles.projectInfo}>
+                  <p className={styles.projectName}>{project.title}</p>
+                  <p className={styles.projectTech}>{project.tech}</p>
+                  <p className={styles.projectDescription}>
+                    {project.description}
+                  </p>
+                  <nav className={styles.projectLinks}>
+                    <ul>
+                      {project.github && (
+                        <li>
+                          <Link href={project.github}>View on Github</Link>
+                        </li>
+                      )}
+                      {project.live && (
+                        <li>
+                          <Link href={project.live}>View live</Link>
+                        </li>
+                      )}
+                    </ul>
+                  </nav>
+                </section>
+              </li>
+            </>
           ))}
         </ul>
       </section>
