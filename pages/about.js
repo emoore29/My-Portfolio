d1b2470de6fe8330e8fe4ab00287880f0d1d6e9b
AthenasCode <emoore5203@gmail.com>
2024-08-14 12:49:15 +0800

    update about

--- pages/about.js
+++ pages/about.js
@@ -6,21 +6,22 @@ export default function About() {
     <Layout>
       <div className={styles.aboutContent}>
         <p>
-          My name is Emma, and I live in Perth, Western Australia. I currently
-          work as a document editor and writer, working on a range of documents
-          from legal transcripts to research manuscripts. I enjoy learning about
-          technology and have worked on a variety of web development and home
-          networking projects.
+          My name is Emma, and I live in Perth, Western Australia. I'm an
+          experienced document editor, working on a range of documents from
+          legal transcripts to research manuscripts. In 2022, I became
+          interested in learning programming as a hobby, and after spending some
+          time creating various projects, I decided to take it seriously as a
+          career path. I'm currently completing a Graduate Diploma in
+          Information Technology, which I hope to complete by September 2024.
         </p>
-
         <p>
           Outside of work and programming, I enjoy running, drawing, and
-          spending time with friends and family. I love animals and between 2018
-          and 2019 I fostered six dogs. I currently have a cat named Penny.
+          spending time with friends and family. I love animals; between 2018
+          and 2019 I fostered and rehomed six shelter dogs. In 2020, my partner
+          and I adopted a cat named Penny.
         </p>
         <p>
-          I designed and developed this website. The artwork you see on my
-          website is made by{" "}
+          I designed and developed this website. Artwork is by{" "}
           <a href="https://www.transhumans.xyz/about" target="_blank">
             Pablo Stanley
           </a>
@@ -29,7 +30,7 @@ export default function About() {
         <figure className={styles.illustration}>
           <img
             src="/images/puppy-transhumans.png"
-            alt="A girl hugging a puppy."
+            alt="Drawing of a girl cuddling a puppy."
           />
           <figcaption className={styles.caption}>
             <a href="https://www.transhumans.xyz/about">Transhumans</a>
